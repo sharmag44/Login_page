e3b622bc9efc28e06cabd9ac5744ac002b08dc93
sharmag44 <62098467+sharmag44@users.noreply.github.com>
2021-08-05 20:14:50 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./Form";
+import Login from "./Login";
+
+var userIsRegistered = false;
+
+function App() {
+  return (
+    <div className="container">{userIsRegistered ? <Login /> : <Form />}</div>
+  );
+}
+
+export default App;
